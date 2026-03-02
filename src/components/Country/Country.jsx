b9d7f3e1ec8c7c3931671ee46983b79a8d79629c
HKDigital360 <hkibria528@gmail.com>
2026-03-03 00:55:40 +0600

    4rth commit

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './Country.css'
+const Country = ({ country }) => {
+    // const currencies = country.currencies.currencies;
+    console.log(country);
+    const [visited, setVisited] = useState(false);
+    const handelVisited = ()=>{
+    //    setVisited(true)
+    if(visited){
+        setVisited(false)
+    }
+    else{
+        setVisited(true )
+    }
+    }
+    // console.log(country.currencies.currencies)
+    return (
+        <div className={`country  ${visited && 'country-visited'}`}>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name : {country.name.common}</h3>
+            <p>Population : {country.population.population}</p>
+            <p>Region : {country.region.region} </p>
+            <p>Currencies Name : {(Object.values(country?.currencies?.currencies)?.[0]?.name)}  </p>
+            <p>Currencies Symbol : {(Object.values(country?.currencies?.currencies)?.[0]?.symbol)}  </p>
+            <p>Area : {country.area.area} {country.area.area > 30000 ? "big country" : "small country"} </p>
+            <button onClick={handelVisited}>
+               {visited? "Visited" : "Not Visited"}  </button>
+
+
+
+        </div>
+    );
+};
+
+export default Country;
--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -1,15 +1,22 @@
 
 
 import React, { use } from 'react';
-
+import Country from '../Country/Country';
+import './Countries.css'
 const Countries = ({countriesPromise}) => {
     const countriesData= use(countriesPromise);
     const allCountries =countriesData.countries;
-    console.log(allCountries);
+    // console.log(allCountries);
 
     return (
         <div>
             <h1>in the countries : {allCountries.length} </h1>
+        <div className='countries'>
+             {
+            allCountries.map(country=> <Country
+                key={country.cca3.cca3} country={country}></Country>)
+         }
+        </div>
         </div>
     );
 };
